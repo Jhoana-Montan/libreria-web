a225c79f52367799b7b29e533722dc2af7d1ae8c
Jhoa <jhoana.mery.montan.garcia@gmail.com>
2026-07-01 08:27:09 -0400

    Version final Libreria Web con Docker

--- web/eliminar.php
+++ web/eliminar.php
@@ -4,9 +4,16 @@ include("conexion.php");
 
 $id = $_GET['id'];
 
+// Eliminar primero las ventas relacionadas
+$conn->query("
+DELETE FROM ventas
+WHERE producto_id = $id
+");
+
+// Luego eliminar el producto
 $conn->query("
 DELETE FROM productos
-WHERE id=$id
+WHERE id = $id
 ");
 
 header("Location: productos.php");
